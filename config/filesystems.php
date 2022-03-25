df0d72671b42e8da383ec5ebe7bc334fa019a43b
Zakaria Santoso <santosozakaria29@gmail.com>
2022-03-25 08:28:14 +0700

    added google drive service provider

--- config/filesystems.php
+++ config/filesystems.php
@@ -28,6 +28,8 @@ return [
     |
     */
 
+    'cloud' => 'google',
+
     'disks' => [
 
         'local' => [
@@ -51,6 +53,14 @@ return [
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+        ],
+
+        'google' => [
+            'driver' => 'google',
+            'clientId' => env('GOOGLE_DRIVE_CLIENT_ID'),
+            'clientSecret' => env('GOOGLE_DRIVE_CLIENT_SECRET'),
+            'refreshToken' => env('GOOGLE_DRIVE_REFRESH_TOKEN'),
+            'folderId' => env('GOOGLE_DRIVE_FOLDER_ID'),
         ],
 
     ],
